390e3246eba3c0e2bc7b6328610234bb645a64d1
SSyeed <syeedsyed2018@gmail.com>
2021-06-10 03:28:57 +0530

    Assignment 1

--- SProject9/app9/views.py
+++ SProject9/app9/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def showIndex(request):
-    data = {"name":"Syeed","Salary":100000}
-    return render(request, 'index.html',data)
+    return render(request, 'index.html')
 
 def registerUser(request):
-    f_name = request.POST.get("x1")
-    l_name = request.POST.get("x2")
-    full_name = f_name + " " + l_name
-    #greet = "Welcome "
-    #resp = greet + full_name
-    #return HttpResponse(resp)
-    ###
-    data = {"fullname":full_name}
-    return render(request, 'welcome.html',data)
-    ###
+    userid = request.POST.get("x1")
+    password = request.POST.get("x2")
+
+    ulist = {"Syeed":"12345","Naveen":"12345","Alex":"54321"}
+
+    if ulist[userid] == password:
+        data = {"user":userid}
+        return render(request,"welcome.html",data)
+    else:
+        return render(request,"error.html")
 
