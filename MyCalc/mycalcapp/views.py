dcaba665576e1ac84977525c66f7f8a205780cfc
SSyeed <syeedsyed2018@gmail.com>
2021-06-12 03:43:52 +0530

    MyCalc

---
+++ MyCalc/mycalcapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+def showCalc(request):
+    if request.method=="POST":
+        fno = int(request.POST.get("f1"))
+        sno = int(request.POST.get("s1"))
+        opr = request.POST.get("opr")
+        print(fno)
+        print(sno)
+        print(opr)
+
+        if(opr == "+"):
+            result = fno + sno
+            print(result)
+            return render(request, "index.html", {"opr": result})
+        elif(opr == "-"):
+            result = fno - sno
+            return render(request, "index.html", {"opr": result})
+        elif(opr == "*"):
+            result = fno * sno
+            return render(request, "index.html", {"opr": result})
+        elif(opr == "%"):
+            result = fno/sno
+            return render(request, "index.html", {"opr": result})
+
+
+
+    if request.method=="GET":
+        return render(request,"index.html")
